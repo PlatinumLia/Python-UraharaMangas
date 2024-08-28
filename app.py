93dc8fbf87edf5c40a5f3a8ad68901e1e456a4ec
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-08-28 11:57:04 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 
+mangás = [{"nome":"Bleach", "volume":"páginas", "em estoque":True},
+          {"nome":"Aku no Hana", "volume":"páginas", "em estoque":True},
+          {"nome":"Black Clover", "volume":"páginas", "em estoque":False}];
+
 def mostra_titulo():
     print("""
         ██████████████████████████████████████████████████████████████████████▀█████████████
@@ -15,6 +19,31 @@ def mostra_escolhas():
     print("4. sair");
 
 def escolhe_opcao():
+    
+    def exibir_subtitulo(texto):
+        os.system("cls");
+        print(texto);
+        print("");
+        
+    def retorna_menu():
+        input("Aperte qualquer tecla para voltar ao menu principal");
+        main();
+
+    def cadastra_manga():
+        exibir_subtitulo("Cadastro de novos mangás");
+        nome_manga = input("Digite o nome do mangá que deseja cadastrar: \n");
+        mangás.append(nome_manga);
+        print(f"O mangá {nome_manga} foi cadastrado com sucesso\n");
+        retorna_menu();
+        
+    def listar_manga():
+        exibir_subtitulo("Lista de mangás cadastrados\n");
+        for mangá in mangás:
+            nome_mangá = mangá["nome"];
+            volume_mangá = mangá["volume"];
+            em_estoque = mangá["em estoque"];
+            print(f"{nome_mangá} | {volume_mangá} | {em_estoque}");
+        retorna_menu();
 
     def finalizar_programa():
         os.system("cls");
@@ -22,16 +51,16 @@ def escolhe_opcao():
     
     def opcao_invalida():
         print("Está é uma opção inválida, escolha outra opção\n");
-        input("Aperte qualquer tecla para voltar ")
-        main()
+        input("Aperte qualquer tecla para voltar ");
+        main();
 
     try:
         opcao_escolhida = int(input("Escolha uma opção: "));
 
         if opcao_escolhida == 1:
-            print("Você escolheu cadastrar o mangá");
+            cadastra_manga();
         elif opcao_escolhida == 2:
-            print("Você escolheu listar o mangá");
+            listar_manga();
         elif opcao_escolhida == 3:
             print("Você escolheu ativar a ID do mangá");
         elif opcao_escolhida == 4:
