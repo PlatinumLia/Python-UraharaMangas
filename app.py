741488419ebb38e11b53164dd849f9278075f481
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-08-30 10:40:52 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 mangás = [{"nome":"Bleach", "volume":"páginas", "ativo":True},
-          {"nome":"Aku no Hana", "volume":"páginas", "ativo":True},
+          {"nome":"Aku no Hana", "volume":"páginas", "ativo":False},
           {"nome":"Black Clover", "volume":"páginas", "ativo":True}];
 
 def mostra_titulo():
@@ -15,18 +15,21 @@ def mostra_titulo():
 def mostra_escolhas():
     print("1. Cadastrar mangá");
     print("2. Listar mangá");
-    print("3. Ativar ID de identificação do mangá");
+    print("3. Ativar/Desativar ID de identificação do mangá");
     print("4. sair");
 
 def escolhe_opcao():
     
     def exibir_subtitulo(texto):
-        os.system("clear");
+        os.system("cls");
+        linha = "-" * len(texto);
+        print(linha);
         print(texto);
+        print(linha);
         print("");
         
     def retorna_menu():
-        input("Aperte qualquer tecla para voltar ao menu principal");
+        input("Aperte qualquer tecla para voltar ao menu principal ");
         main();
 
     def cadastra_manga():
@@ -45,27 +48,31 @@ def escolhe_opcao():
         for mangá in mangás:
             nome_mangá = mangá["nome"];
             volume_mangá = mangá["volume"];
-            ativo = mangá["ativo"];
-            print(f"{nome_mangá} | {volume_mangá} | {ativo}");
+            ativo = "Ativado" if mangá["ativo"] else "Desativado";
+            print(f"{nome_mangá.ljust(20)} | {volume_mangá.ljust(20)} | {ativo.ljust(20)}");
+            print()
         retorna_menu();
     
     def ativar_identificacao():
-        exibir_subtitulo("Ativar identificação do mangá");
-        nome_manga = input("Digite o nome do mangá que deseja ativar a identificação: ");
+        exibir_subtitulo("Ativar/Desativar identificação do mangá");
+        nome_manga = input("Digite o nome do mangá que deseja ativar ou desativar a identificação: ");
 
         manga_encontrado = False;
+        
         for manga in mangás:
-            if  nome_manga == manga["nome"]:
-                manga_encontrado = False;
+            if nome_manga == manga["nome"]:
+                manga_encontrado = True;
                 manga["ativo"] = not manga["ativo"];
                 mensagem = f"O mangá {nome_manga} foi ativado com sucesso" if manga["ativo"] else f"O mangá {nome_manga} foi desativado com sucesso";
                 print(mensagem);
-    
-        if not manga_encontrado:
-            print("O mangá não foi encontrado");
 
+            if not manga_encontrado:
+                print("O mangá não foi encontrado");
+
+        retorna_menu()
+        
     def finalizar_programa():
-        os.system("clear");
+        os.system("cls");
         print("Finalizando o programa\n");
     
     def opcao_invalida():
@@ -81,7 +88,7 @@ def escolhe_opcao():
         elif opcao_escolhida == 2:
             listar_manga();
         elif opcao_escolhida == 3:
-            print("Você escolheu ativar a ID do mangá");
+            ativar_identificacao();
         elif opcao_escolhida == 4:
             finalizar_programa();
         else:
